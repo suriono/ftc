35059982090d0cd02fdd5807ade05bca90d5b00a
Usman Suriono <suriono@yahoo.com>
2018-10-09 15:02:03 -0500

    remove chassis_motor from IMU debug

--- ftc_app2018/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Debug/Debug_IMU.java
+++ ftc_app2018/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Debug/Debug_IMU.java
@@ -2,11 +2,7 @@ package org.firstinspires.ftc.teamcode.Debug;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.Library.Chassis_Motors;
 import org.firstinspires.ftc.teamcode.Library.Rev_IMU;
-
-import java.util.Locale;
 
 
 @TeleOp(name="Debug: IMU", group="Debugging")
