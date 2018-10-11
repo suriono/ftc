bbc797d62023e2591affa091bb0e67f5f7e5f3a2
Usman Suriono <suriono@yahoo.com>
2018-10-11 14:25:04 -0500

    Add comments to pixy analog

--- ftc_app2018/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library/PixyCam_Analog.java
+++ ftc_app2018/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library/PixyCam_Analog.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+// Use this one when setting the PixyMon to "analog/digital x"
+
 public class PixyCam_Analog  {
     private DigitalChannel        digIn_detected_pixy;                // Device Object
     private AnalogInput           analogIn_pixy;
@@ -13,10 +15,13 @@ public class PixyCam_Analog  {
         analogIn_pixy       = hardwareMap.get(AnalogInput.class, "pixy_analog");
     }
 
-    public boolean isObjectDetected()  {      // if object is detected
+    // reading from pin 1 (SPI MISO, UART Rx) from Pixy
+    public boolean isObjectDetected()  {      // true if object is detected
         return digIn_detected_pixy.getState();
     }
-    public double getAnalogRead() {
+
+    // reading from pin 3 (Analog out) from Pixy
+    public double getAnalogRead() {   // from 0 to 3.3V where 1.15V when the object is right in front of it
         return  analogIn_pixy.getVoltage();
     }
 }
